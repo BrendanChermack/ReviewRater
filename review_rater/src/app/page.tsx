025953dc1876f5a235b0aa6f3ddecbce6604ca4a
“BrendanChermack” <ChermackBrendan@gmail.com>
2023-10-29 18:55:36 -0500

    added hello world

---
+++ review_rater/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex justify-center">
+      <div className="font-mono text-sm lg:flex">
+        <h1>Hello World!</h1>
+      </div>
+    </main>
+  )
+}
